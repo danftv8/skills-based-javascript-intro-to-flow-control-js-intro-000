f24f0cfda6713f16810d63f27eb69b8ceddd8709
Daniel Mullaney <danftv@gmail.com>
2021-01-10 19:43:56 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,8 +24,8 @@ function ageChecker(age) {
 
 function ternaryTeenager(age) {
   return age > 12 || age < 20 ?
-  "You are not a teenager" :
-  "You are a teenager"
+  "You are a teenager" :
+  "You are not a teenager"
 }
 
 function switchAge(age) {
