e42443602f1dcb428cc4ff0a713d9e6027359a2e
Daniel Mullaney <danftv@gmail.com>
2021-01-10 19:34:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,7 @@
 function basicTeenager(age) {
   if (age >= 13 || age <= 19) {
     'You are a teenager!'
+    return true
   }
 }
 
@@ -27,8 +28,36 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  switch (expression) {
-    case n:
-  }
+  switch (age) {
+    case 13:
+    console.log("You are a teenager");
+    break;
+
+    case 14:
+    console.log("You are a teenager");
+    break;
+
+    case 15:
+    console.log("You are a teenager");
+    break;
+
+    case 16:
+    console.log("You are a teenager");
+    break;
+
+    case 17:
+    console.log("You are a teenager");
+    break;
+
+    case 18:
+    console.log("You are a teenager");
+    break;
+    
+    case 19:
+    console.log("You are a teenager");
+    break;
+  default:
+    console.log("You have an age");
+}
 
 }
